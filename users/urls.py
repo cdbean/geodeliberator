c856076c1e0a8aef3004022dc76384eba7e8a976
Dong Chen <dong@Dongs-MacBook-Pro.local>
2013-05-01 09:19:53 -0400

    v1.0

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls.defaults import *
 from views import * 
 
 urlpatterns = patterns('',
-    url(r'login^$', login),
+    url(r'^login$', login_user),
     url(r'^register$', register),
+    url(r'^logout$', logout_user),
 )
 
  
